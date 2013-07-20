9cd0ec3dcf3f9dde2a0ed60bd051fef90e50f251
Vivian <vivian.cung@gmail.com>
2013-07-20 15:07:53 -0400

    test

---
+++ submitForm.php
@@ -0,0 +1,61 @@
+
+<?php
+
+  $email_to = $_REQUEST['recipient'];
+    $email_subject = $_REQUEST['subject'];
+
+
+    $first_name = $_REQUEST['first_name']; 
+    $last_name = $_REQUEST['last_name']; 
+    $email_from = $_REQUEST['email']; 
+    $educator = $_REQUEST['educator']; 
+    $zip = $_REQUEST['zip']; 
+	$study = $_REQUEST['study'];
+    $degrees = $_REQUEST['degrees']; 
+	$website = $_REQUEST['website']; 
+    $whenwhere = $_REQUEST['whenwhere'];
+	$image = $_REQUEST['image'];
+	$message = $_REQUEST['message'];
+
+	$error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+	
+	//make sure email does not have inappropriate characters.
+	//might want to do this for all variables for security
+	if(!preg_match($email_exp,$email_from)) {
+		$error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+    }
+	function clean_string($string) {
+		$bad = array("content-type","bcc:","to:","cc:","href");
+		return str_replace($bad,"",$string);
+    }
+     
+	$email_message = "First Name: ".clean_string($first_name)."\n";
+    $email_message .= "Last Name: ".clean_string($last_name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Are you an Educator: ".clean_string($educator)."\n";
+	$email_message .= "When & Where: ".clean_string($whenwhere)."\n";
+    $email_message .= "Current zip/REQUESTal code: ".clean_string($zip)."\n";
+    $email_message .= "Major/Field of Study: ".clean_string($study)."\n";
+    $email_message .= "Degrees: ".clean_string($degrees)."\n";
+    $email_message .= "Website: ".clean_string($website)."\n";
+  
+  //  $email_message .= "Have any photos with you and Jules Engel that you would like to share?: ".clean_string($website)."\n";
+    $email_message .= "Please talk about your experience with Jules Engel and how:\n".clean_string($message)."\n";
+	
+	
+
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);  
+
+?>
+ 
+<!-- include your own success html here -->
+ 
+Thank you for your submission...
+ 
+<?php
+
+?>
